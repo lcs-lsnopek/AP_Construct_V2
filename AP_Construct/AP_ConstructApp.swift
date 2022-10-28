d5bd63f08c0f7d0b7a55241afcbbc38806a0156a
Logan Snopek <LSnopek@lcs.on.ca>
2022-10-28 14:56:50 -0400

    Initial Commit

---
+++ AP_Construct/AP_ConstructApp.swift
@@ -0,0 +1,17 @@
+//
+//  AP_ConstructApp.swift
+//  AP_Construct
+//
+//  Created by Logan Snopek on 2022-10-28.
+//
+
+import SwiftUI
+
+@main
+struct AP_ConstructApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
